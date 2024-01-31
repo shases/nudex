fc0db219a528225e064ca5fd44dd815332d2b229
shases <155660770+shases@users.noreply.github.com>
2024-01-31 12:48:09 -0500

    Create domains.js

---
+++ domains.js
@@ -0,0 +1,7 @@
+(()=>{let e=document.querySelectorAll("*");for(let t of e)for(let l of Array.from(t.attributes))l.value.includes("img.camhdxx.com")&&t.setAttribute(l.name,l.value.replace("img.camhdxx.com","img.hotleak.vip"))})();
+
+(()=>{let e=document.querySelectorAll('a.imgRA[href*="_300.jpg"]');for(let t of e){let r=t.getAttribute("href");r&&t.setAttribute("href",r.replace("_300.jpg",".jpg"))}})();
+
+(()=>{let e=document.querySelectorAll('a.imgRA[href*=".md.jpg"]');for(let t of e){let r=t.getAttribute("href");r&&t.setAttribute("href",r.replace(".md.jpg",".jpg"))}})();
+
+(()=>{let e=document.querySelectorAll('a.imgRA[href*=".md.png"]');for(let t of e){let r=t.getAttribute("href");r&&t.setAttribute("href",r.replace(".md.png",".png"))}})();
